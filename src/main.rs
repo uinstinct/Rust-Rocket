71decd24e3f088d96f3272418bc828c3c0bc78ca
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-08-10 00:50:23 +0530

    rocket ignites

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() ->&'static str {
+    "Master Blaster"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
