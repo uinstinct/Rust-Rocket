8c917001d87d76af13911cf6bab41fe6b5a4d9d0
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-08-10 01:44:20 +0530

    send json response

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::content::Json;
+use rocket::response::status;
 
 #[get("/")]
-fn index() ->&'static str {
+fn index() -> &'static str {
     "Master Blaster"
+}
+
+#[get("/<id>")]
+fn read(id: usize) -> status::Accepted<String> {
+    status::Accepted(Some(format!("id: {}", id)))
+}
+
+// struct Message<'r> {
+//     id: Option<i64>,
+//     message: &'r str,
+// }
+
+#[get("/j")]
+fn json() -> Json<&'static str> {
+    Json(
+        "{
+        'status': 'success',
+        'message': 'Hello API!'
+      }",
+    )
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, read, json])
 }
